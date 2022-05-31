61879c7e49aab257bf1fc0d93221c41604335118
theandersonn <theandersonn@gmail.com>
2022-05-31 11:30:31 -0300

    refactor: insere metodos em classe

--- v1/controllers/quote-controller.js
+++ v1/controllers/quote-controller.js
@@ -1,21 +1,17 @@
 const Quote = require('../models/Quote');
 
-const createQuote = (req, res) => {
-  res.render('createquote');
-};
+module.exports = class QuoteController {
+  static createQuote = (req, res) => {
+    res.render('createquote');
+  };
 
-const createQuoteSave = async (req, res) => {
-  const { description, author } = req.body;
-  await Quote.create({ description, author });
-  res.redirect('/quotes');
-};
+  static createQuoteSave = async (req, res) => {
+    const { description, author } = req.body;
+    await Quote.create({ description, author });
+    res.redirect('/quotes');
+  };
 
-const getAllQuotes = async (req, res) => {
-  res.render('home');
-};
-
-module.exports = {
-  getAllQuotes,
-  createQuote,
-  createQuoteSave
+  static getAllQuotes = async (req, res) => {
+    res.render('home');
+  };
 };
--- v1/routes/quotes-routes.js
+++ v1/routes/quotes-routes.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {  
-  getAllQuotes,
-  createQuote,
-  createQuoteSave
-} = require('../controllers/quote-controller');
+const QuoteController = require('../controllers/quote-controller');
 
-router.get('/', getAllQuotes);
-router.get('/create', createQuote);
-router.post('/create', createQuoteSave);
+router.get('/', QuoteController.getAllQuotes);
+router.get('/create', QuoteController.createQuote);
+router.post('/create', QuoteController.createQuoteSave);
 
 module.exports = router;
