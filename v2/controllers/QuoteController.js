43d60030b3b325c041f5e03109f5e5a2184fe8ef
theandersonn <theandersonn@gmail.com>
2022-06-13 19:55:13 -0300

    v2: estrutra de visualização do criar

--- v2/controllers/QuoteController.js
+++ v2/controllers/QuoteController.js
@@ -3,5 +3,9 @@ const QuoteController = require('../models/Quote');
 module.exports = class QuoteController {
   static showQuotes(req, res) {
     res.render('quotes/all');
-  }
+  };
+
+  static createQuote(req, res) {
+    res.render('quotes/create');
+  };
 };
--- v2/routes/quotesRoutes.js
+++ v2/routes/quotesRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const QuoteController = require('../controllers/QuoteController');
 
+router.get('/create', QuoteController.createQuote);
 router.get('/', QuoteController.showQuotes);
 
 module.exports = router;
